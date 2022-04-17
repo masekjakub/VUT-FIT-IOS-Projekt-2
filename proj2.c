4f973919eaf179a32d18fe627f5410b43e107259
masekjakub <masekjak02@gmail.com>
2022-04-17 03:02:30 +0200

    bruh

--- proj2.c
+++ proj2.c
@@ -12,10 +12,26 @@
 sem_t *semaphorOxy = NULL;
 sem_t *semaphorHyd = NULL;
 sem_t *writeSem = NULL;
-FILE *outF;
-int *row = NULL;
+sem_t *moleculeSem = NULL;
+sem_t *moleculeDoneSem = NULL;
 
-int parseInt(char *src, long *dest)
+FILE *outF;
+struct shared_t
+{
+    int row;
+    int numOfOxy;
+    int numOfHyd;
+    int creating;
+    long no;
+    long nh;
+
+};
+
+struct shared_t *shared = NULL;
+int *moleculeId = NULL;
+
+    int
+    parseInt(char *src, long *dest)
 {
     char *ptr;
     *dest = strtol(src, &ptr, 10);
@@ -42,16 +58,20 @@ void clear()
     sem_close(semaphorOxy);
     sem_close(semaphorHyd);
     sem_close(writeSem);
+    sem_close(moleculeSem);
+    sem_close(moleculeDoneSem);
     sem_unlink("/xmasek19.IOS.Projekt2.O");
     sem_unlink("/xmasek19.IOS.Projekt2.H");
     sem_unlink("/xmasek19.IOS.Projekt2.Write");
+    sem_unlink("/xmasek19.IOS.Projekt2.Molecule");
+    sem_unlink("/xmasek19.IOS.Projekt2.MoleculeDone");
     fclose(stdout);
 }
 
 int init()
 {
     // outF = fopen("proj2.out", "w");
-    if ((semaphorOxy = sem_open("/xmasek19.IOS.Projekt2.O", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED)
+    if ((semaphorOxy = sem_open("/xmasek19.IOS.Projekt2.O", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
     {
         printf("Semaphore O not created\n");
         return EXIT_FAILURE;
@@ -64,6 +84,16 @@ int init()
     if ((writeSem = sem_open("/xmasek19.IOS.Projekt2.Write", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
     {
         printf("Semaphore write not created\n");
+        return EXIT_FAILURE;
+    }
+    if ((moleculeSem = sem_open("/xmasek19.IOS.Projekt2.Molecule", O_CREAT | O_EXCL, 0666, 2)) == SEM_FAILED)
+    {
+        printf("Semaphore molecule not created\n");
+        return EXIT_FAILURE;
+    }
+    if ((moleculeDoneSem = sem_open("/xmasek19.IOS.Projekt2.MoleculeDone", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED)
+    {
+        printf("Semaphore moleculeDone not created\n");
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
@@ -79,21 +109,87 @@ void mysleep(int max)
 }
 void handleOxygen(int id, int ti, int tb)
 {
+    sem_wait(writeSem);
+    printf("%d: O %d: started\n", shared->row += 1, id);
+    sem_post(writeSem);
+    shared->numOfOxy++;
+
     mysleep(ti);
     sem_wait(writeSem);
-    printf("%d: O %d: going to queue\n", *row += 1, id);
+    printf("%d: O %d: going to queue\n", shared->row += 1, id);
     sem_post(writeSem);
+
+    //wait until other molecule is created
     sem_wait(semaphorOxy);
+    sem_wait(moleculeSem);
+    printf("Ox: %d\n", shared->numOfOxy);
+    sem_wait(moleculeSem);
+    //start creating
+    if (shared->numOfHyd<2){
+        sem_wait(writeSem);
+        printf("%d: O %d: not enough H \n", shared->row += 1, id);
+        sem_post(writeSem);
+        sem_post(semaphorHyd);
+        exit(0);
+    }
+    sem_wait(writeSem);
+    printf("%d: O %d: creating molecule %d\n", shared->row += 1, id, *moleculeId += 1);
+    sem_post(writeSem);
+    shared->numOfOxy--;
+    shared->creating = 2;
+
+    sem_post(semaphorHyd);
+    sem_post(semaphorHyd);
+    mysleep(tb);
+    //created molecule
+    sem_wait(writeSem);
+    printf("%d: O %d: molecule %d created\n", shared->row += 1, id, *moleculeId);
+    sem_post(writeSem);
+
+    sem_post(moleculeDoneSem);
+    sem_post(moleculeDoneSem);
+
     exit(0);
 }
 
 void handleHydrogen(int id, int ti, int tb)
 {
+    sem_wait(writeSem);
+    printf("%d: H %d: started\n", shared->row += 1, id);
+    sem_post(writeSem);
+    shared->numOfHyd++;
+
     mysleep(ti);
     sem_wait(writeSem);
-    printf("%d: H %d: going to queue\n", *row += 1, id);
+    printf("%d: H %d: going to queue\n", shared->row += 1, id);
     sem_post(writeSem);
     sem_wait(semaphorHyd);
+
+    // start creating
+    if (shared->numOfHyd < 2 || shared->numOfOxy == 0)
+    {
+            sem_wait(writeSem);
+            printf("%d: O %d: not enough O or H \n", shared->row += 1, id);
+            sem_post(writeSem);
+            exit (0);
+    }
+    sem_wait(writeSem);
+    printf("%d: H %d: creating molecule %d\n", shared->row += 1, id, *moleculeId);
+    sem_post(writeSem);
+    shared->numOfHyd--;
+
+    sem_wait(moleculeDoneSem);
+    sem_wait(writeSem);
+    printf("%d: H %d: molecule %d created\n", shared->row += 1, id, *moleculeId);
+    sem_post(writeSem);
+
+    sem_post(moleculeSem);
+    shared->creating--;
+    if (shared->creating == 0)
+    {
+        sem_post(semaphorOxy);
+    }
+
     exit(0);
 }
 
@@ -125,8 +221,12 @@ int main(int argc, char **argv)
 
     pid_t pidParent, pid;
     pidParent = getpid();
-    row = map(row);
-    *row = 0;
+    shared = map(shared);
+    moleculeId = map(moleculeId);
+    shared->row = 0;
+    shared->no = no;
+    shared->nh = nh;
+    *moleculeId = 0;
     if (init())
     {
         exit(EXIT_FAILURE);
@@ -137,9 +237,6 @@ int main(int argc, char **argv)
         pid = fork();
         if (pid == 0) // only child
         {
-            sem_wait(writeSem);
-            printf("%d: H %d: started\n", *row += 1, i);
-            sem_post(writeSem);
             handleHydrogen(i, ti, tb);
         }
         else if (pid < 0) // error occured
@@ -155,9 +252,6 @@ int main(int argc, char **argv)
         pid = fork();
         if (pid == 0) // only child
         {
-            sem_wait(writeSem);
-            printf("%d: O %d: started\n", *row += 1, i);
-            sem_post(writeSem);
             handleOxygen(i, ti, tb);
         }
         else if (pid < 0) // error occured
@@ -166,24 +260,22 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }
     }
+    munmap(shared, sizeof *shared);
+    exit(0);
 
     sleep(1);
     int num;
     sem_getvalue(semaphorOxy, &num);
     printf("No O start: %d\n", num);
 
-    sem_post(semaphorOxy);
-    sem_post(semaphorHyd);
-    sem_post(semaphorHyd);
-
     sem_getvalue(semaphorOxy, &num);
     printf("No O end: %d\n", num);
     sleep(1);
     sem_getvalue(semaphorOxy, &num);
     printf("No O end: %d\n", num);
-    printf("Row: %d\n", *row);
+    printf("Row: %d\n", shared->row);
 
-    munmap(row, sizeof *row);
+    
     clear();
     printf("End\n");
     exit(0);
